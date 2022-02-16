c2f934bdc4acb61cce54b3c8523d4cffdbe03d18
jchy <recjaswantcse@gmail.com>
2022-02-16 16:47:30 +0530

    crash fixed

--- app/utils/fileUpload.js
+++ app/utils/fileUpload.js
@@ -3,7 +3,8 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     filename: function(req,file, callback){
-          callback(null, file.originalname)
+        callback(null,  new Date().toISOString() + file.originalname)
+        //   callback(null, file.originalname)
     },
     destination: function(req,file,callback){
         console.log(`destination: ${path.join(__dirname,"../uploads/")}`)
